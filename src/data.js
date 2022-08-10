0e8a0d70b98c7f3b23b597224255363a3653d94a
Youssef <ym05831@gmail.com>
2022-08-10 14:23:22 +0300

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,75 @@
+export const projectData = [
+  {
+    projectName: "Bitcoin Miner",
+    projectDate: "Feb 2021",
+    projectDescription:
+      "A Bitcoin miner projet that uses python to generate a SHA256 hash",
+    tags: ["Python"],
+    repo: "https://github.com/JosephMoussa/BitcoinMiner",
+  },
+  {
+    projectName: "Article Scraper",
+    projectDate: "Feb 2021",
+    projectDescription:
+      "A web scraper using beatiful soup for random wikipedia articles ",
+    tags: ["Python"],
+    repo: "https://github.com/JosephMoussa/wiki_article_scraper",
+  },
+  {
+    projectName: "Android Music Player",
+    projectDate: "Sep 2021",
+    projectDescription:
+      "A Music Player made using java with android studio which has basic features.",
+    tags: ["Java"],
+    repo: "https://github.com/JosephMoussa/Music-Player",
+  },
+  {
+    projectName: "Bitcoin Price Alert",
+    projectDate: "March 2022",
+    projectDescription:
+      "live bitcoin price alert with sound with a time delay of 10 seconds using an API.",
+    tags: ["Python", "API"],
+    repo: "https://github.com/JosephMoussa/live_BitCoinPrice_alert",
+  },
+  {
+    projectName: "YuTalks",
+    projectDate: "Sep 2022",
+    projectDescription:
+      "A Speech-to-Text and Text-to-Speech converter using python with Tkinter.",
+    tags: ["Python"],
+    repo: "https://github.com/JosephMoussa/YuTalks",
+  },
+  {
+    projectName: "Minimal Portfolio",
+    projectDate: "Aug 2022",
+    projectDescription:
+      "A minimalistic portfolio free to use for anyone with clean detail and crisp front-end.",
+    tags: ["HTML5", "CSS", "Javascript"],
+    repo: "https://github.com/JosephMoussa/MinimalPortfolio",
+  },
+  {
+    projectName: "Text Based Game",
+    projectDate: "Aug 2022",
+    projectDescription:
+      "A text based adventure game made using python with excellent object oriented plan.",
+    tags: ["Python"],
+    repo: "https://github.com/JosephMoussa/Text-Based-Game",
+  },
+  {
+    projectName: "Speed Type Test",
+    projectDate: "Aug 2022",
+    projectDescription:
+      "A typing test using javascript, HTML and CSS with more then 1 mode to try.",
+    tags: ["HTML5, CSS3, Javascript"],
+    repo: "https://github.com/JosephMoussa/typespeed-test",
+  },
+  {
+    projectName: "Snake Game",
+    projectDate: "Feb 2021",
+    projectDescription:
+      "Snake game made using java with excellent planning for objects.",
+    tags: ["Java"],
+    repo: "https://github.com/JosephMoussa/Snake_Game",
+  },
+  
+];
